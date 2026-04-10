cb75de176a2a9c4f9dd0721ba637c53afd686884
LDilks <ldilks@udel.edu>
2026-04-10 14:02:35 -0400

    Added 8 Directional Movement

--- pair-project/player.gd
+++ pair-project/player.gd
@@ -18,11 +18,18 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	var x_direction := Input.get_axis("move_left", "move_right")
+	if x_direction:
+		velocity.x = x_direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	var y_direction := Input.get_axis("move_up", "move_down")
+	if y_direction:
+		velocity.y = y_direction * SPEED
+	else:
+		velocity.y = move_toward(velocity.x, 0, SPEED)
+	
 	
 	if Input.is_action_just_pressed("interact"):
 		nearby_interactibles[-1].interact();
