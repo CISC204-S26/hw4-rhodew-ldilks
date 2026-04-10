f86e6a096349437ab6310caf93be41267df6dc64
LDilks <ldilks@udel.edu>
2026-04-10 14:19:53 -0400

    Made the Interactible its own scene

--- pair-project/player.gd
+++ pair-project/player.gd
@@ -1,34 +1,32 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const speed = 200.0
 const JUMP_VELOCITY = -400.0
 
 #variables
 var nearby_interactibles = [];
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	#if not is_on_floor():
-		#velocity += get_gravity() * delta
-
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var x_direction := Input.get_axis("move_left", "move_right")
-	if x_direction:
-		velocity.x = x_direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	var direction = Vector2.ZERO #set direction var
 	
-	var y_direction := Input.get_axis("move_up", "move_down")
-	if y_direction:
-		velocity.y = y_direction * SPEED
-	else:
-		velocity.y = move_toward(velocity.x, 0, SPEED)
+	#movement input
+	if Input.is_action_pressed("move_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("move_right"):
+		direction.x += 1
+	if Input.is_action_pressed("move_up"):
+		direction.y -= 1
+	if Input.is_action_pressed("move_down"):
+		direction.y += 1
+	
+	direction = direction.normalized() #normalize diagnols
+	velocity = direction * speed #apply movement
+	move_and_slide()
 	
 	
 	if Input.is_action_just_pressed("interact"):
