174ff8ce7c35a813b0d5244250cf368392939c62
LDilks <ldilks@udel.edu>
2026-04-10 14:22:09 -0400

    Switch Script

--- pair-project/switch.gd
+++ pair-project/switch.gd
@@ -1,13 +1,17 @@
-class_name Interactable extends Area2D
+class_name Switch extends Area2D
 
 @export var interaction_name = "test_interaction"
 @export var itneraction_type = "test"
 @export var display_active = false;
 
-var is_interacted: bool = false;
+var is_switched: bool = false;
+
+signal switcheroo
 
 func interact():
-	is_interacted = !is_interacted
+	is_switched = !is_switched
+	print_debug(is_switched)
+	switcheroo.emit(is_switched)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
