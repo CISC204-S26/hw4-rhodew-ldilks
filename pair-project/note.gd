ee449ae08dadfcf9c157b662cb44179b0880517a
rhodew <rhodew@udel.edu>
2026-04-15 14:41:34 -0400

    note progress
    now triggers right and shows + hides the note when interact. hides note if player walks away

--- pair-project/note.gd
+++ pair-project/note.gd
@@ -11,6 +11,7 @@ signal reading
 
 func interact():
 	is_interacted = !is_interacted
+	reading.emit(is_interacted)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -20,3 +21,9 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+
+func _on_body_exited(body: Node2D) -> void:
+	is_interacted = false
+	reading.emit(is_interacted)
---
+++ pair-project/paper.gd
@@ -0,0 +1,19 @@
+extends TextureRect
+
+var paper_visible = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_note_reading(paper_visible) -> void:
+	if paper_visible == false:
+		hide()
+	else:
+		show()
